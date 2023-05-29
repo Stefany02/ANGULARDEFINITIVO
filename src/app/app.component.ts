5e9124c6571da6b43c9bea73f1734b86f9ac8699
Stefany <donstefany@gmail.com>
2023-05-29 00:30:26 -0300

    ...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { DataService } from './services/data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  providers: [DataService]
 })
 export class AppComponent {
   title = 'portfolio';
-}
+  constructor(private dataSvc: DataService) {}
+  ngOnInit(){
+    }
+  }
+
--- src/app/model/usu.ts
+++
@@ -1,6 +0,0 @@
-export class usu {
-    token!: string;
-    type!: string;
-    nombreUsuario! : string;
-    authorities!: string[];
-}
